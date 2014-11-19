83f29ad6c30cb6042f39d0faeec92e97928233fd
Spencer Lynn <slynn@3-gis.com>
2014-11-19 12:18:10 -0600

    Lesson 4 complete: will not run so commented out of HTML

--- koans/lesson4-Events.js
+++ koans/lesson4-Events.js
@@ -2,32 +2,30 @@ module('Lesson 4 - Events');
 
 test('listening to events', function() {
     var received = '';
-    var subscription =
-        $(document)
-            .toObservable('foo')
-            .subscribe(function(e) { received += e.payload; });
-    
-    $(document).trigger({ type: 'foo', payload : 'M'});
-    $(document).trigger({ type: 'foo', payload : 'A'});
-    $(document).trigger({ type: 'foo', payload : 'T'});
+    var subscription = $(document)
+      .toObservable('foo')
+      .subscribe(function(e) { received += e.payload; });
+
+    $(document).trigger({ type: 'foo', payload: 'M' });
+    $(document).trigger({ type: 'foo', payload: 'A' });
+    $(document).trigger({ type: 'foo', payload: 'T' });
     subscription.dispose();
-    $(document).trigger({ type: 'foo', payload : 'T'});
-    
-    equals(received, 'MAT'/*_______*/);
+    $(document).trigger({ type: 'foo', payload: 'T' });
+
+    equals(received, 'MAT');
 });
 
 test('listening to the right events', function() {
     var received = '';
-    var subscription =
-        $(document)
-            .toObservable('foo')
-            .subscribe(function(e) { received += e.payload; });
-    
-    $(document).trigger({ type: 'foo', payload : 'M'});
-    $(document).trigger({ type: 'bar', payload : 'A'});
-    $(document).trigger({ type: 'foo', payload : 'T'});
-    $(document).trigger({ type: 'foo', payload : 'T'});
+    var subscription = $(document)
+      .toObservable('foo')
+      .subscribe(function(e) { received += e.payload; });
+
+    $(document).trigger({ type: 'foo', payload: 'M' });
+    $(document).trigger({ type: 'bar', payload: 'A' });
+    $(document).trigger({ type: 'foo', payload: 'T' });
+    $(document).trigger({ type: 'foo', payload: 'T' });
     subscription.dispose();
-    
-    equals(received, 'MTT'/*_______*/);
+
+    equals(received, 'MTT');
 });
